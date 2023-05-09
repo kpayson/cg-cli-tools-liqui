ce07034e63c940b34685d26c7e91ad9b62e6afd6
Ken Payson <kenneth.payson@axleinfo.com>
2023-05-09 12:47:57 -0400

    fix: debug Input_Database_Name

--- entrypoint.sh
+++ entrypoint.sh
@@ -132,7 +132,7 @@ export DB_USER=$(echo $credentials | jq -r ".username") && \
 export DB_PASSWORD=$(echo $credentials | jq -r ".password") && \
 export DB_PORT=$(echo $credentials | jq -r ".port") && \
 ls /liquibase/changelog && \
-liquibase --url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE_NAME} --username=${DB_USER} --password=${DB_PASSWORD} --changeLogFile="/liquibase/changelog/changelog.xml" update'
+echo INPUT_DATABASE_NAME=$INPUT_DATABASE_NAME
   cf run-and-wait $CF_APP   "$LIQUIBASE_ENTRY_COMMAND"
 
 }
